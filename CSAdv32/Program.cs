f16d9931720048d67f7f1aecbd62fef6c2a81501
Seyeon Park <psyvitamin@gmail.com>
2024-08-13 09:53:04 +0900

    제네릭 실습 1

--- CSAdv32/Program.cs
+++ CSAdv32/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace CSAdv32
 {
+
+    class Wanted<T>  // Generic 
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            Value = value;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Wanted<int> wantedInt = new Wanted<int>(65535);
+            Wanted<string> wantedString = new Wanted<string>("Hello, World!");
+            Wanted<double> wantedDouble = new Wanted<double>(3.14159);
+
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
